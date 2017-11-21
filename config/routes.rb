0d3392bef0b1eeea06daef97b07f0a8b3b991ed9
Arthur Thieffry <arthieffry@gmail.com>
2017-11-21 18:20:52 +0100

    Ajout de UsersController + Model User + bug

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :articles
 
